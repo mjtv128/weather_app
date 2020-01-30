11a1e38264d7925c411b3b12cdfba6a9600c170f
Michelle Van <mjtvan@gmail.com>
2020-01-30 01:29:03 +0000

    added user input

--- app.js
+++ app.js
@@ -1,14 +1,28 @@
-const request = require('request')
 const geocode = require('./utils/geocode')
 const forecast = require("./utils/forecast");
 
-geocode('Philadelphia', (error, data) => {
-    console.log("Error", error)
-    console.log("Data", data)
-})
+const address = process.argv[2]
+// console.log(process.argv);
+
+if (address) {
+    geocode(address, (error, data) => {
+      if (error) {
+        return console.log(error);
+      }
+
+      forecast(data.lat, data.long, (error, forecastData) => {
+        if (error) {
+          return console.log(error);
+        }
+        console.log(data.location);
+        console.log(forecastData);
+      });
+    });
+} else {
+    console.log("Please provide an address")
+}
 
 
-forecast(-75.7088, 44.1545, (error, data) => {
-  console.log('Error', error)
-  console.log('Data', data)
-})
+
+
+
