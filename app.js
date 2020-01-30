9560b49da5ad0d5b4dc36c47092b49d41e31a00c
Michelle Van <mjtvan@gmail.com>
2020-01-30 00:49:54 +0000

    added geocode callback

--- app.js
+++ app.js
@@ -1,19 +1,36 @@
 const request = require('request')
+const geocode = require('./utils/geocode')
+
 const url =
   "https://api.darksky.net/forecast/3ff4d2a990752fe3b6f00446000da200/37.8267,-122.4233";
 
-request({url:url, json: true}, (error, response, body) => {
-    //we would like a request to parse it as json
-    //therefore no need to parse it
-    console.log(`It is currently ${body.currently.temperature}. THere is a ${body.currently.precipProbability}% chance of rain`)
+// request({url:url, json: true}, (error, response, body) => {
+//     if (error){
+//         console.log("Unable to connect to weather service")
+//     } else if (body.error) {
+//         console.log('Unable to find location')
+//     } else{
+//         console.log(`It is currently ${body.currently.temperature}. There is a ${body.currently.precipProbability}% chance of rain`)
+//           }
+// })
+
+// const geoURL =
+//   "https://api.mapbox.com/geocoding/v5/mapbox.places/12what.json?access_token=pk.eyJ1IjoidGVybWluYWwxMjM0IiwiYSI6ImNrNXpqZ3E5czFpYTUzZ3BvbDRjZWxsMGwifQ.XVAbSXiwYCLUjibRE2wvrQ&limit=1";
+
+// request({url: geoURL, json: true}, (error, response, body) => {
+//       if (error){
+//           console.log("Unable to connect to location services")
+//       } else if (body.features.length === 0){
+//           console.log("No matching results")
+//       } else {
+//          console.log(body.features[0].center);
+//       }
+// })
+
+
+
+geocode('Philadelphia', (error, data) => {
+    console.log("Error", error)
+    console.log("Data", data)
 })
-
-const geoURL =
-  "https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoidGVybWluYWwxMjM0IiwiYSI6ImNrNXpqZ3E5czFpYTUzZ3BvbDRjZWxsMGwifQ.XVAbSXiwYCLUjibRE2wvrQ&limit=1";
-
-request({url: geoURL, json: true}, (error, response, body) => {
-      console.log(body.features[0].center)
-      //[0] because it is in the array
-})
-
 
